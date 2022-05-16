460bc18201b3dc46176c933838495fd603d7322b
vaarg <104344158+vaarg@users.noreply.github.com>
2022-05-16 20:14:39 +1000

    Update Acarine.py

--- Acarine.py
+++ Acarine.py
@@ -1,6 +1,6 @@
 #!/bin/python
 
-# Acarine v0.4.1
+# Acarine v0.4.2
 
 # Usage: 
     # Acarine.py -t [IP] -p [PORT]
@@ -32,6 +32,8 @@ class color:
    UNDERLINE = '\033[4m'
    END = '\033[0m'
 
+# [@] - Common Functions:
+
 def bufferSend(buffer):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
@@ -43,6 +45,14 @@ def bufferSend(buffer):
     except:
         print("Error: Could not connect.\n")
         return 1
+
+def checklist():
+    print(f"""\nRecommended pre-testing checklist:
+    [*] Run Immunity as Admininistrator
+    [*] Set Immunity's working directory [{color.BOLD}{color.YELLOW}!mona config -set workingfolder c:\mona\%p{color.END}]
+    [*] Load and Run the vulnerable program in Immunity
+    [*] Disable 'Real-time protection' in Windows Security""")
+    menu()
 
 def countAdd():
     global count
@@ -85,6 +95,21 @@ def menuCheck(input):
         menu()
     else:
         True
+
+def offsetAvailble():
+    try:
+        offset
+    except NameError:
+        offsetEnter()
+
+def offsetEnter():
+    global offset
+    try:
+        offset = int(input(f"{color.RED}\nNo offset variable detected from previous sections.{color.END}\n\n{color.GREEN}Enter offset: {color.END}"))
+        return offset
+    except:
+        print(f"{color.RED}\nOffset must be an Integer! Try again!{color.END}")
+        offsetEnter()
 
 # [1] Is a Buffer Overflow Possible?:
 
@@ -173,21 +198,6 @@ This means we are on target and now we will move on to the finding Bad Character
     
 # [3] Finding Bad Characters:
 
-def offsetAvailble():
-    try:
-        offset
-    except NameError:
-        offsetEnter()
-
-def offsetEnter():
-    global offset
-    try:
-        offset = int(input(f"{color.RED}\nNo offset variable detected from previous sections.{color.END}\n\n{color.GREEN}Enter offset: {color.END}"))
-        return offset
-    except:
-        print(f"{color.RED}\nOffset must be an Integer! Try again!{color.END}")
-        offsetEnter()
-
 def charsMod(charsResultInput):
     global badChars
     toRemove = charsResultInput.split(' ')
@@ -228,7 +238,7 @@ we are testing has any {color.BOLD}Bad Characters{color.END} that need to be eli
     ## Tells how to make first byte array
     print(f"\nFirst we need to generate a byte array. This will be used to determine any Bad Characters.\n")
     print(rf'To generate a byte array in Immunity enter "{color.YELLOW}!mona bytearray -b "\x00"{color.END}"')
-    print("""\nAgain, make sure to RE-LOAD and RE-RUN the program you are testing in Immunity.\n
+    print(f"""\nAgain, make sure to {color.BOLD}RE-LOAD and RE-RUN{color.END} the program you are testing in Immunity.\n
 When ready, a payload of characters will be sent to the target program.\n""")
     ## Test 1: w/o user input:
     charsInitialInput = input(f"{color.GREEN}[ENTER] to Continue: {color.END}")
@@ -247,7 +257,7 @@ After making note of the bad character(s), make sure you {color.BOLD}RE-LOAD and
     badCharsExec(charsResultInput)
     # Test 3: Asks user to either enter if unmodified or enter badchars:
     while True:
-        print(rf"Using the new ESP, enter '{color.YELLOW}!mona compare -f C:\mona\{color.END}{color.RED}<PROGRAM>{color.END}{color.YELLOW}\bytearray.bin -a {color.END}{color.RED}<ESP>{color.END}' again.")
+        print(rf"Using the {color.BOLD}new ESP{color.END}, enter '{color.YELLOW}!mona compare -f C:\mona\{color.END}{color.RED}<PROGRAM>{color.END}{color.YELLOW}\bytearray.bin -a {color.END}{color.RED}<ESP>{color.END}' again.")
         newCharsResultInput = input(f"\n{color.GREEN}If 'Status' header now returns 'Unmodified' press [ENTER] (or enter remaining BadChars): {color.END}")
         if newCharsResultInput != "":
             badCharsExec(newCharsResultInput)
@@ -262,19 +272,13 @@ def jumpPointTest():
     print(badChars)
     sys.exit()
 
-# [5] Final Buffer Overflow Exploit
+# [5] Final Buffer Overflow Exploit:
 
 def exploitBuffer():
     print("Exploit Placeholder")
     sys.exit()
 
-def checklist():
-    print(f"""\nRecommended pre-testing checklist:
-    [*] Run Immunity as Admininistrator
-    [*] Set Immunity's working directory [{color.BOLD}{color.YELLOW}!mona config -set workingfolder c:\mona\%p{color.END}]
-    [*] Load and Run the vulnerable program in Immunity
-    [*] Disable 'Real-time protection' in Windows Security""")
-    menu()
+# [!] Init:
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-t','-T','--target',help="target IP")#,default=socket.gethostname())
@@ -283,11 +287,7 @@ args = parser.parse_args()
 
 HOST = args.target
 PORT = args.port
-# badChars = r"\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\x3e\x3f\x40\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5a\x5b\x5c\x5d\x5e\x5f\x60\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x7b\x7c\x7d\x7e\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff"
 count = 0
-
-# def argChecker(HOST,PORT):
-#     if HOST and/or PORT is "None" then throw "-h" error.
 
 print(f"""{color.BOLD}{color.UNDERLINE}\nWelcome to Acarine!\n{color.END}
 This program is a {color.BOLD}Buffer Overflow{color.END} utility. 
